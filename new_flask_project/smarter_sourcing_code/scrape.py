e0ef7759827b202761cf7844ab52cb78f49a4742
Alex Corrigan <104227360+acorrig50@users.noreply.github.com>
2024-11-02 18:58:05 -0400

    Add files via upload

--- new_flask_project/smarter_sourcing_code/scrape.py
+++ new_flask_project/smarter_sourcing_code/scrape.py
@@ -6,18 +6,18 @@ import numpy as np
 def the_scraping(input_query):
     # start with the URLS and the different versions, the difference in the URLS to note is that the _sop variable changes its number
     # 1. Best match
-    best_match_preowned = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + input_query + "&_sacat=260010&_sop=12&LH_ItemCondition=4"
-    best_match_new = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query +"&_sacat=260010&LH_ItemCondition=1000%7C1750&_sop=12"
+    best_match_preowned = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + input_query + "&_sop=12&LH_ItemCondition=4"
+    best_match_new = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query +"&LH_ItemCondition=1000%7C1750&_sop=12"
     # sold URLs
-    best_match_sold_preowned = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query +"&_sacat=260010&_sop=12&LH_Sold=1&LH_Complete=1&rt=nc&LH_ItemCondition=3000"
-    best_match_sold_new = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query +"&_sacat=260010&_sop=12&LH_Sold=1&LH_Complete=1&rt=nc&LH_ItemCondition=1000%7C1750"
+    best_match_sold_preowned = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query +"&_sop=12&LH_Sold=1&LH_Complete=1&rt=nc&LH_ItemCondition=3000"
+    best_match_sold_new = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query +"&_sop=12&LH_Sold=1&LH_Complete=1&rt=nc&LH_ItemCondition=1000%7C1750"
 
     # 2. Low to high
-    low_to_high_preowned = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + input_query + "&_sacat=0&_sop=15&rt=nc&LH_ItemCondition=4"
-    low_to_high_new = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query + "&_sacat=0&_sop=15&rt=nc&LH_ItemCondition=3"
+    low_to_high_preowned = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + input_query + "&_sop=15&rt=nc&LH_ItemCondition=4"
+    low_to_high_new = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query + "&_sop=15&rt=nc&LH_ItemCondition=3"
     # sold urls
-    low_to_high_preowned_sold = "https://www.ebay.com/sch/11554/i.html?_from=R40&_nkw="+ input_query +"&_blrs=recall_filtering&_sop=15&LH_ItemCondition=3000&rt=nc&LH_Sold=1&LH_Complete=1"
-    low_to_high_new_sold = "https://www.ebay.com/sch/11554/i.html?_from=R40&_nkw="+ input_query +"&_blrs=recall_filtering&_sop=15&LH_Sold=1&LH_Complete=1&rt=nc&LH_ItemCondition=1000%7C1750"
+    low_to_high_preowned_sold = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query +"&_blrs=recall_filtering&_sop=15&LH_ItemCondition=3000&rt=nc&LH_Sold=1&LH_Complete=1"
+    low_to_high_new_sold = "https://www.ebay.com/sch/i.html?_from=R40&_nkw="+ input_query +"&_blrs=recall_filtering&_sop=15&LH_Sold=1&LH_Complete=1&rt=nc&LH_ItemCondition=1000%7C1750"
 
     # Now we grab the number of items listed for both URL
     # The element is a h1 class
@@ -40,14 +40,14 @@ def the_scraping(input_query):
     
     # store the results of each query in a list
     url_storage = [
-        first, 
-        second, 
-        third, 
-        fourth, 
-        fifth, 
-        sixth, 
-        seventh, 
-        eighth
+        first, # 0
+        second, # 1
+        third, # 2
+        fourth, # 3
+        fifth, # 4
+        sixth, # 5
+        seventh, # 6
+        eighth # 7
     ]
     text = []
     values = []
@@ -58,9 +58,14 @@ def the_scraping(input_query):
         golden_goose = soup.find("h1", {'class':'srp-controls__count-heading'})
         text.append(golden_goose.get_text().split(" "))
         values.append(float(text[i][0].replace('+','').replace(",",'')))
-        
-        
+    
     print(values)
-    search_query = input_query
+    
+    best_match_preowned_rate = values[1] / values[0]
+    best_match_new_rate = values[3] / values[2]
+    low_to_high_preowned_rate = values[5] / values[4]
+    low_to_high_new_rate = values[7] / values[6]
+    
+    return best_match_preowned_rate, best_match_new_rate, low_to_high_preowned_rate, low_to_high_new_rate
 
-the_scraping("american eagle kick boot jeans")
+the_scraping("patagonia synchilla pullover mens")
